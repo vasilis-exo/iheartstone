f25a4268bf3d2b3f3ce83c356cb7db7ad8e16588
Vasilis Macbook <exochidis@gmail.com>
2018-09-10 21:47:43 +0300

    ionic skeleton

---
+++ dist/out-tsc/src/app/card-deck/card-deck.page.spec.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var testing_1 = require("@angular/core/testing");
+var card_deck_page_1 = require("./card-deck.page");
+describe('CardDeckPage', function () {
+    var component;
+    var fixture;
+    beforeEach(testing_1.async(function () {
+        testing_1.TestBed.configureTestingModule({
+            declarations: [card_deck_page_1.CardDeckPage],
+            schemas: [core_1.CUSTOM_ELEMENTS_SCHEMA],
+        })
+            .compileComponents();
+    }));
+    beforeEach(function () {
+        fixture = testing_1.TestBed.createComponent(card_deck_page_1.CardDeckPage);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+    it('should create', function () {
+        expect(component).toBeTruthy();
+    });
+});
+//# sourceMappingURL=card-deck.page.spec.js.map
---
+++ dist/out-tsc/src/environments/environment.uat.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.environment = {
+    production: false,
+    test: true,
+    dev: false
+};
+//# sourceMappingURL=environment.uat.js.map
