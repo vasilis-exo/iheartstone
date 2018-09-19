c684879ff22e6c36050905c779219fde0473fa47
Vasilis Macbook <exochidis@gmail.com>
2018-09-19 18:59:04 +0300

    remopve dist folder

--- dist/out-tsc/src/environments/environment.prod.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    production: true,
-    test: false,
-    dev: false
-};
-//# sourceMappingURL=environment.prod.js.map
--- dist/out-tsc/src/environments/environment.uat.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    production: false,
-    test: true,
-    dev: false
-};
-//# sourceMappingURL=environment.uat.js.map
